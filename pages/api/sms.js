e43be27bd53a6fd94fc4cc1b45c75324df90385b
Aymen Ben Amor <aymen.benamor@richemont.com>
2022-03-01 17:40:07 +0100

    test

--- pages/api/sms.js
+++ pages/api/sms.js
@@ -2,6 +2,6 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 export default function handler(req, res) {
     const twiml = new MessagingResponse();
-    twiml.message('TwilioQuest rules');
+    twiml.message('This is a test and TwilioQuest rules is the right answer!');
     res.status(200).setHeader('Content-Type', 'text/xml').send(twiml.toString());
 }
